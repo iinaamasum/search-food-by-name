11342bed5663dc27616232a2e6b4db308b09a794
iinaamasum <iinaamasum@gmail.com>
2022-02-26 04:53:37 +0600

    almost__done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
+document.getElementById('spinner__div').style.display = 'none';
 document.getElementById('button__feild').addEventListener('click', () => {
-  const input = document.getElementById('button__feild').value;
-  document.getElementById('button__feild').value = '';
+  document.getElementById('spinner__div').style.display = 'block';
+  const input = document.getElementById('input__feild').value;
+  document.getElementById('input__feild').value = '';
+  document.getElementById('card__show').textContent = '';
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
     .then((res) => res.json())
     .then((data) => {
       // console.log(info.meals);
-      for (const info of data.meals) {
-        const col = document.createElement('col');
-        console.log(info);
-        col.innerHTML = `
+      console.log(data);
+      if (data.meals == null) {
+        console.log('nonr');
+        const col = document.createElement('div');
+        col.classList.add('col-12');
+        col.textContent = '';
+        // console.log(info);
+        col.innerHTML = `<h1 class="text-center">Data not found. The food is either finished or you entered wrong name.</h1>
+        `;
+        document.getElementById('card__show').appendChild(col);
+        document.getElementById('spinner__div').style.display = 'none';
+      } else {
+        for (const info of data.meals) {
+          const col = document.createElement('div');
+          col.classList.add('col');
+          col.textContent = '';
+          console.log(info);
+          col.innerHTML = `
         <div class="card h-100">
           <img src="${info.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body">
             <h4 class="card-title">Food title: ${info.strMeal}</h4>
             <p class="card-text">${info.strInstructions.slice(0, 150)}...</p>
-            <button class="btn btn-outline-success shadow-none col-12" type="button" id="button__feild__two">Know More</button>
+            <a href= "#card__details__img" class="btn btn-outline-success shadow-none col-12" type="button" onclick="foodDetails(${
+              info.idMeal
+            })">Know More</a>
           </div>
           <div class="card-footer bg-white d-flex justify-content-between">
             <small class="text-muted">Origin: ${info.strArea}</small>
@@ -22,7 +41,38 @@ document.getElementById('button__feild').addEventListener('click', () => {
           </div>
         </div>
         `;
-        document.getElementById('card__show').appendChild(col);
+          document.getElementById('card__show').appendChild(col);
+          document.getElementById('spinner__div').style.display = 'none';
+        }
       }
     });
 });
+
+const foodDetails = (info) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${info}`)
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById('card__details').textContent = '';
+      for (const info of data.meals) {
+        const col = document.createElement('div');
+        col.classList.add('col');
+        col.textContent = '';
+        console.log(info.idMeal);
+        col.innerHTML = `
+        <div id="card__details__img" class="card h-100 my-4">
+        <img src="${info.strMealThumb}">
+        <div class="card-body">
+          <h4 class="card-title">Food title: ${info.strMeal}</h4>
+          <p class="card-text">${info.strInstructions}</p>
+        </div>
+        <a href="${info.strYoutube}" target="_blank" ><button class="btn btn-outline-success shadow-none col-12 my-2">See Video</button> </a>
+        <div class="card-footer bg-white d-flex justify-content-between">
+          <small class="text-muted">Origin: ${info.strArea}</small>
+          <small class="text-muted">Catagory: ${info.strCategory}</small>
+        </div>
+      </div>
+      `;
+        document.getElementById('card__details').appendChild(col);
+      }
+    });
+};
